f2939eb1eeb703395ad501080618b3b8cf1e6206
d4wner <d4wner@gmail.com>
2014-08-06 17:29:32 +0800

    file_modify

--- attack.py
+++ attack.py
@@ -5,38 +5,63 @@ import sys
 import urllib2
 import urllib
 import os
+import threading
 sys.path.append("plugin/")
-import color_pr
-global site
-def start():
+#import color_pr
+global editor_types
+types=["fckeditor","ewebeditor","kindeditor"]
+
+
+class timer(threading.Thread): #The timer class is derived from the class threading.Thread  
+    def __init__(self, cmd):  
+        threading.Thread.__init__(self)  
+        self.cmd = cmd  
+    def run(self): #Overwrite run() method, put what you want the thread do here  
+		try:
+			os.system(self.cmd)
+		except Exception,e:
+			print e
+			#break
+
+def scan_run(dicname):
+	pack = dicname+"/crack.py"
+	for site in open('dic/url.txt','r'):
+		cmd="python "+pack+" "+site
+		#print cmd
+	try:
+		tscan=timer(cmd)
+		tscan.start()
+	except Exception,e:
+		print e
+
+def choose_scan_start(editor_types):
+	#types=[]
+	if "," in editor_types and editor_types != "":
+		for item in editor_types.strip().split(","):
+			if item in types:
+				scan_run(item)
+	elif editor_types != "":
+		if item in types:
+			scan_run(editor_types)
+	else:
+		multi_scan_start()
+
+
+def multi_scan_start():
 	fold_list_tmp=[]
 	fold_list_tmp=os.listdir(".")
 	#print fold_list_tmp
 	for dicname in fold_list_tmp:
-		if not os.path.isfile(dicname) and dicname !=".git" and dicname != "plugin":
-			print dicname
-			#sys.path.append(dicname+"/")
-			#print dicname+"/"
-			pack = dicname+"/crack.py"
-			os.system("python "+pack+" "+site)
-			for filename in os.listdir(dicname):
-				print color_pr.green(filename)
-				#pack = dicname+"/crack.py"
-				#print pack
-				#os.popen('python '+pack)
-				#site = "!!!!"
-				#os.system("python "+pack+" "+site)
-
-
+		if not os.path.isfile(dicname) and dicname !=".git" and dicname != "plugin" and dicname != "dic":
+			scan_run(dicname)
 
 
 if __name__ == "__main__":
 	print ("""       ===================================
        +-----------DemonSpider-----------+
-       +--My dream,my morning sunshine.--+
-       +--------Editor-Assasin-V1.0------+
+       +--------Editor-Assasin-V2.0------+
        +--------demon@dawner.info--------+
-       +--Exp:url-->http://www.baidu.com-+
+       +url.txt:http://www.baidu.com/fck/+
        ===================================
                     =     =
                    ==     ==
@@ -45,9 +70,9 @@ if __name__ == "__main__":
                     =======
                      =====
                       ===
-                       +\nPlease input the url or input "x" to use the default dic:\n<-|->""")
-	site = raw_input()
-	start()
+                       +\nPlease check the default dics and you can input the editor type to start:\n<-|->""")
+	editor_types = raw_input()
+	choose(editor_types)
 
         
 
