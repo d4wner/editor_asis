722ef0cdf625d7a86d782c8291f8924e117ce014
d4wner <d4wner@gmail.com>
2014-08-12 17:30:09 +0800

    test

--- test/upload.py
+++ test/upload.py
@@ -1,15 +1,21 @@
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
 import urllib2
+import re
+
 
 register_openers()
 
 
-datagen, headers = multipart_encode({"userfile": open("1.jpg", "rb")})
+datagen, headers = multipart_encode({"NewFile": open("1.jpg", "rb")})
 
 
-request = urllib2.Request("http://localhost/testphp.php", datagen, headers)
+request = urllib2.Request("http://localhost/editor/filemanager/browser/default/browser.html?Type=all&Connector=connectors/php/connector.php", datagen, headers)
 
-print urllib2.urlopen(request).read()
-
+resp= urllib2.urlopen(request).read()
+print resp
+#xx= re.findall(r'\(.*jpg',resp)
+##print xx[0]
+#print xx[0][1:].split(',"')[0]
+#print xx[0][1:].split(',"')[1]
 
